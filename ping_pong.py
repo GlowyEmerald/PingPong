4bc8ed3ddd4321cda70f933dc79d682501736aad
GlowyEmerald <rafi.zeman@gmail.com>
2025-05-26 20:30:45 +0200

    ping_pong.py v0.1

--- ping_pong.py
+++ ping_pong.py
@@ -14,8 +14,10 @@ fps = 60
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
+        
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
+        
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -25,18 +27,30 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x >= self.speed:
+        if keys[K_w] and self.rect.y >= self.speed:
             if not keys[K_RIGHT]:
-                self.rect.x -= self.speed
-        elif keys[K_RIGHT] and self.rect.x <= win_width - (self.width + self.speed):
-            self.rect.x += self.speed
+                self.rect.y -= self.speed
+        elif keys[K_s] and self.rect.y <= win_width - (self.width + self.speed):
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y >= self.speed:
+            if not keys[K_RIGHT]:
+                self.rect.y -= self.speed
+        elif keys[K_DOWN] and self.rect.y <= win_width - (self.width + self.speed):
+            self.rect.y += self.speed
         
 
     def fire(self):
         bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -15)
         bullets.add(bullet)
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis_ball.png',200,200,4,50,50)
 
 while Game:
     for e in event.get:
