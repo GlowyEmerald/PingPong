445b1befcfc57e5e47a1bbe7bebf9f95050e52ef
GlowyEmerald <rafi.zeman@gmail.com>
2025-05-26 20:47:53 +0200

    ping_pong.py 0.1.2

--- ping_pong.py
+++ ping_pong.py
@@ -12,10 +12,10 @@ clock = time.Clock()
 fps = 60
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
         
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = transform.scale(image.load(player_image), (width, height))
         self.speed = player_speed
         
         self.rect = self.image.get_rect()
